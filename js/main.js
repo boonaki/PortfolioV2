50756191154ca16a0e5ab167d495ad19144d8287
boonaki <joshdurr17@gmail.com>
2022-07-14 15:02:21 +0200

    multi-browser support

--- js/main.js
+++ js/main.js
@@ -13,15 +13,3 @@ let animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'https://assets7.lottiefiles.com/packages/lf20_exfaib4n.json' // lottie file path
   })
-
-//   let arrOfProjects = document.getElementsByClassName('project')
-// //   arrofProjects.forEach((project) => project.addEventListener('focus', turnColorBlack))
-//   for(let i = 0; i < arrOfProjects.length; i++){
-//       arrOfProjects[i].addEventListener('mouseenter', turnColorBlack(i))
-//   }
-
-
-//   function turnColorBlack(i){
-//     let colorChange = document.getElementsByClassName('projectP')
-//     console.log(colorChange[i])
-//   }
